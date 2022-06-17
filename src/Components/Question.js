d7f72315197f1e551f19dd7f7784d40cc0379488
Ritish jaiswal <ritishjaiswal20@gmail.com>
2022-06-17 12:38:23 +0530

    implenting wrong ans and right answer logic

---
+++ src/Components/Question.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import ErrorMessage from './ErrorMessage';
+import "./Question.css"
+function Question({currQues,setCurrQues,questions,options,correct,setScore,score,setQuestions}) {
+   const[selected,setSelected]=useState();
+   const[error,setError]=useState(false);
+    
+   const handleSelect=(i)=>{
+      if(selected===i && selected===correct){
+        return "select";
+      }else if(selected===i && selected!==correct){
+        return "wrong";
+      }else if(i=== correct){
+        return "select";
+      }
+   };
+
+  const handleCheck=(i)=>{
+    setSelected(i);
+    if(i===correct) setScore(score+1);
+    setError(false);
+  };
+
+   return (
+    <div  className="question">
+        <h1>Question {currQues +1}</h1>
+        <div className="singleQuestion">
+            <h2>{questions[currQues].question}</h2>
+            <div className="options">
+                {error && <ErrorMessage>{error}</ErrorMessage>}
+                {
+                    options && options.map((i)=><button
+                    onClick={()=>handleCheck(i)}
+                    className={`singleOption ${selected && handleSelect(i)}`}
+                    key={i}
+                    disabled={selected}
+                    >{i}</button>)
+                }
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Question
--- src/Pages/Quiz.js
+++ src/Pages/Quiz.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
-
+import { CircularProgress } from '@mui/material';
+import "./Quiz.css"
+import Question from '../Components/Question';
 function Quiz({ name, questions, score, setScore, setQuestions }) {
     const [options, setOptions] = useState();
     const [currQues, setCurrQues] = useState(0);
@@ -21,7 +23,34 @@ function Quiz({ name, questions, score, setScore, setQuestions }) {
   
     return (
       <div className="quiz">
-        quiz
+          <span className="subtitle">Welcome,{name}</span>
+         {
+          questions? (
+                 <>
+                     <div className="quizInfo">
+                        <span>{questions[currQues].category}</span>
+                          <span>Score:{score}</span>
+                     </div>
+                     <Question
+                       currQues={currQues}
+                       setCurrQues={setCurrQues}
+                       questions={questions}
+                       options={options}
+                       correct={questions[currQues]?.correct_answer}
+                       score={score}
+                       setScore={setScore}
+                       setQuestions={setQuestions}                  
+                     />
+                 </>
+          ):(
+            <CircularProgress 
+            style={{margin:100}}
+            color="inherit"
+            size={150}
+            thickness={1}
+            />
+          )
+         }
       </div>
     );
 }
