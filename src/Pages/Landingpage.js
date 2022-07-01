a879612d5153eaa82d88097b3a7a2a11a5da52c6
Ritish jaiswal <ritishjaiswal20@gmail.com>
2022-07-01 17:09:45 +0530

    modal added

--- src/Pages/Landingpage.js
+++ src/Pages/Landingpage.js
@@ -7,7 +7,50 @@ import { Link, Navigate, useNavigate } from 'react-router-dom';
 import Footer from "../Components/Footer";
 import firebase from "firebase";
 import db from "../firebase";
+import Modal from "react-modal";
+import { containerClasses } from '@mui/system';
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    height: "55%",
+    width: "50%",
+    transitionDuration: "5s",
+    transform: "translate(-50%, -50%)",
+    backgroundImage:
+      "url(https://images.unsplash.com/photo-1614741118887-7a4ee193a5fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNvZGluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60)",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "100% 100%",
+    objectFit: "conatin",
+    overflow:"hidden",
+  },
+  
+};
+
 function Landingpage() {
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+    
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = "#f00";
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+      // setTimeout(() => closeModal(), 3000);
+  }
+
+
      const [input,setInput]=useState("");
      const [message, setMessage] = useState(false);
    let navigate = useNavigate();
@@ -75,30 +118,55 @@ function Landingpage() {
         <div className="landingpage-showcaseItems">
           <div className="landingpage-showcaseItem">
             <h4>Science And Nature</h4>
-            <img src="https://i.gifer.com/7RwC.gif" alt="" onClick={takequiz} />
+            <img
+              src="https://i.gifer.com/7RwC.gif"
+              alt=""
+              onClick={openModal}
+            />
           </div>
           <div className="landingpage-showcaseItem">
             <h4>Computer</h4>
-            <img src="https://i.gifer.com/8oXf.gif" alt="" onClick={takequiz} />
+            <img
+              src="https://i.gifer.com/8oXf.gif"
+              alt=""
+              onClick={openModal}
+            />
           </div>
           <div className="landingpage-showcaseItem">
             <h4>Mathematics</h4>
-            <img src="https://i.gifer.com/Cwgf.gif" alt="" onClick={takequiz} />
+            <img
+              src="https://i.gifer.com/Cwgf.gif"
+              alt=""
+              onClick={openModal}
+            />
           </div>
         </div>
 
         <div className="landingpage-showcaseItems">
           <div className="landingpage-showcaseItem">
             <h4>History</h4>
-            <img src="https://i.gifer.com/g09P.gif" alt="" onClick={takequiz} />
+            <img
+              src="https://i.gifer.com/g09P.gif"
+              alt=""
+              onClick={openModal}
+            />
           </div>
           <div className="landingpage-showcaseItem">
             <h4>Politics</h4>
-            <img src="https://i.gifer.com/7N1e.gif" alt="" onClick={takequiz} />
+            <img
+              src="https://i.gifer.com/7N1e.gif"
+              alt=""
+              onClick={openModal}
+            />
           </div>
           <div className="landingpage-showcaseItem">
             <h4>General Knowledge</h4>
-            <img src="https://i.gifer.com/E2Ak.gif" alt="" onClick={takequiz} />
+            <img
+              src="https://i.gifer.com/E2Ak.gif"
+              alt=""
+              // onClick={takequiz}
+              onClick={openModal}
+            />
           </div>
         </div>
       </div>
@@ -129,6 +197,29 @@ function Landingpage() {
         )}
         <></>
       </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <div className="modal-content">
+          <h1>Hello there</h1>
+          <h2>Take advantage of our free quizzes!</h2>
+          <p>
+            We urge you to take them as a registered user. You will get access
+            to a lot more questions and all results will be tied up to your
+            profile!
+          </p>
+          <div className="button-section">
+            <button className="quizbtnn" onClick={takequiz}>
+              Take Quiz
+            </button>
+            {/* <button className="bttn" onClick={closeModal}>close</button> */}
+          </div>
+        </div>
+      </Modal>
       <Footer />
     </div>
   );
